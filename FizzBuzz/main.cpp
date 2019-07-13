b9387156ef2bc7251b7ff24fc352dfc91ec646bd
WM <graczyk.mateusz@interia.pl>
2019-07-13 17:27:53 +0200

    .

---
+++ FizzBuzz/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::cout;
+
+int main() {
+	for(int i = 1; i <=100; i++) {
+		if( i % 3 == 0 || i % 5 == 0) {
+			if(i % 3 == 0) {
+				cout << "Fizz";
+			}
+			if(i % 5 == 0) {
+				cout << "Buzz";
+			}
+		}
+		else {
+			cout << i;
+		}
+		cout << std::endl;
+	}
+}
